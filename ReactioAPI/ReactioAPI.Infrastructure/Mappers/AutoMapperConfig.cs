a10cd534dc7b539f8a5c49a0cc94eb59443d5812
Adam Sowiński <adam_sowinski@gazeta.pl>
2018-12-17 00:22:54 +0100

    Fixed factors

--- ReactioAPI/ReactioAPI.Infrastructure/Mappers/AutoMapperConfig.cs
+++ ReactioAPI/ReactioAPI.Infrastructure/Mappers/AutoMapperConfig.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using Newtonsoft.Json;
 using ReactioAPI.Core.Domain;
 using ReactioAPI.Infrastructure.DTO;
+using System.Collections.Generic;
 
 namespace ReactioAPI.Infrastructure.Mappers
 {
@@ -9,7 +11,10 @@ namespace ReactioAPI.Infrastructure.Mappers
         public static IMapper Initialize()
             => new MapperConfiguration(cfg =>
             {
-                cfg.CreateMap<Reaction, ReactionDTO>();
+                cfg.CreateMap<Reaction, ReactionDTO>()
+                .ForMember(dest => dest.Factors, opt => opt.MapFrom(src => 
+                !string.IsNullOrWhiteSpace(src.Factor) ? 
+                JsonConvert.DeserializeObject<IEnumerable<Factor>>(src.Factor) : null));
                 cfg.CreateMap<Product, ProductDTO>();
                 cfg.CreateMap<Substrate, SubstrateDTO>();
                 cfg.CreateMap<AppSetting, AppSettingDTO>();
